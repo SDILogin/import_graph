16615df53f6754a452f738375fe1cfbff20d5a47
SDI <diselivanov@gmail.com>
2015-01-23 12:29:07 +0300

    Парсер с графом

--- parser.py
+++ parser.py
@@ -1,4 +1,4 @@
-import re, os
+import re, os, graph
 
 class Parser:
     def __init__(self, module_code):
@@ -26,16 +26,22 @@ if __name__ == '__main__':
     # parser = FileParser('/Users/SDI/Desktop/IOSProjects/vazhno-ios/Vazhno/VZVZRSimpleViewController.m')
     # res = parser.parse_modules_name()
     # print(*res, sep='\n')
-    files_in_dir, full_file_names = [], []
+    files_in_dir, full_file_names, name_full_name_pair = [], [], []
     for root, dirs, file_names in os.walk('/Users/SDI/Desktop/IOSProjects/vazhno-ios/Vazhno'):
         files = [x for x in file_names if x.endswith('.h')]
         full_file_names += [root+'/'+x for x in files]
         files_in_dir +=  files
+        name_full_name_pair += list(zip(files, full_file_names))
     
     G = {}
-    for file_in_dir in full_file_names:
-        parser = FileParser(file_in_dir)
+    for file_in_dir in name_full_name_pair:
+        full_name = file_in_dir[1]
+        short_name = file_in_dir[0]
+        parser = FileParser(full_name)
         imported_modules = parser.parse_modules_name()
-        G[file_in_dir] = imported_modules
+        G[short_name] = imported_modules
 
-    print(G)
+    gr = graph.Graph(G)
+    print('nodes: ', *gr.nodes, sep='\n')
+    print('-'*10)
+    print('edges: ', *[str(x[0])+'->'+str(x[1]) for x in gr.edges], sep='\n')
